b9a249e9cde9f3e54637f9ecabb6b7d925847c7e
Anjali Kasoudhan <anjalikasoudhan1705@gmail.com>
2025-10-11 19:42:46 +0530

    Added entity class for User ,Course,Enrollment,Professor,Chpater and WishList

--- src/main/java/com/courseware/controller/LoginController.java
+++ src/main/java/com/courseware/controller/LoginController.java
@@ -1,5 +1,11 @@
 package com.courseware.controller;
 
-public class LoginController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class LoginController {
+    @Autowired
+    private userService userService;
+    
 }
